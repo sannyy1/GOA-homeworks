df9a0e47a55f03bd7fe8110b835b16d5c4b45065
sannyy1 <alex.maisuradze29@gmail.com>
2024-06-15 20:51:45 +0400

    commit

---
+++ Level 07/homework.py
@@ -0,0 +1,38 @@
+#Excercise 2
+
+#integer
+print(int("90"))
+print(int("55"))
+print(int("15"))
+print(int("13"))
+print(int("87"))
+
+#string
+print(str("Didgori"))
+print(str("1121"))
+print(str("David"))
+print(str("Aghmashenebeli"))
+print(str("True"))
+
+#float
+print(float(13.5))
+print(float(17.0))
+print(float(14.99))
+print(float(19.19))
+print(float(6.66))
+
+
+#Excercise 3
+num1 = input("Please enter any number you want: ")
+num2 = input("Please enter any second number you want: ")
+num3 = input("Please enter any third number you want: ")
+num4 = input("Please enter any fourth number you want: ")
+num5 = input("Please enter any fifth number you want: ")
+
+
+print(5 / 4)
+print(8 // 3)
+print(15 % 4)
+print(87 % 6)
+print(13 // 12)
+print(13 / 12)
