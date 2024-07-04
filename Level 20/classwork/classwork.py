e0ebf21633181d29176fe280096755266afb85e5
sannyy1 <alex.maisuradze29@gmail.com>
2024-07-05 02:58:11 +0400

    commit

---
+++ Level 20/classwork/classwork.py
@@ -0,0 +1,5 @@
+#classwork
+def display_info(name, surname, age):
+    print(name, surname, age)
+
+display_info("Sandor", "Maisuradze", 16)
---
+++ Level 20/homework/homework.py
@@ -0,0 +1,17 @@
+#2
+def hello():
+    print("GOA Best!")
+
+#3
+def sum(a, b):
+    result = (a + b)
+    print("The sum of", a, "and", b, "is:", result)
+
+#4
+def to_string(value):
+    print(str(value))
+    
+to_string = (42)         
+to_string = (3.14)       
+to_string = ("Hello!")   
+to_string = ([1, 2, 3])  
