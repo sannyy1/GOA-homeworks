a1300a896be2ce01037a4249d57356ae95ef0cce
sannyy1 <alex.maisuradze29@gmail.com>
2024-07-16 16:30:54 +0400

    commit

---
+++ Level 28/homework/task1.py
@@ -0,0 +1,8 @@
+#https://www.codewars.com/kata/5583090cbe83f4fd8c000051
+#https://www.codewars.com/kata/53af2b8861023f1d88000832
+#https://www.codewars.com/kata/5672a98bdbdd995fad00000f
+#https://www.codewars.com/kata/5ce9c1000bab0b001134f5af
+#https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
+#https://www.codewars.com/kata/5772da22b89313a4d50012f7
+#https://www.codewars.com/kata/568d0dd208ee69389d000016
+#https://www.codewars.com/kata/57a0885cbb9944e24c00008e
